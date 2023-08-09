7e7456166f993e53ac9e0fc57f1119e6eefbde23
kunalSinghQA <kunal.s@feuji.com>
2023-08-09 16:20:24 +0530

    updating reporter

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+const { defineConfig, devices } = require("@playwright/test");
 
 /**
  * Read environment variables from file.
@@ -11,10 +11,10 @@ const { defineConfig, devices } = require('@playwright/test');
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  testDir: './tests',
-  timeout: 3*60 * 1000,
+  testDir: "./tests",
+  timeout: 3 * 60 * 1000,
   expect: {
-    timeout: 6 * 1000
+    timeout: 6 * 1000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -25,7 +25,9 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: process.env.CI
+    ? [["html", { open: "never" }]]
+    : [["json", { outputFile: "test-results.json" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -33,17 +35,16 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     // trace: 'on-first-retry',
-    headless:true,
-    screenshot:"only-on-failure",
+    headless: true,
+    screenshot: "only-on-failure",
     trace: "on",
-
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     // {
@@ -60,7 +61,6 @@ module.exports = defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-  
   ],
 
   /* Run your local dev server before starting the tests */
@@ -70,4 +70,3 @@ module.exports = defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
